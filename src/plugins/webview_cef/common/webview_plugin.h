c08b61d3d3db2f4583d033ff141ac783511c2565
Bojidar Tonchev <bojidar.tonchev@gmail.com>
2025-03-17 16:22:15 +0200

    + Initial value converting impl

---
+++ src/plugins/webview_cef/common/webview_plugin.h
@@ -0,0 +1,6 @@
+#ifndef WEBVIEW_PLUGIN_H
+#define WEBVIEW_PLUGIN_H
+
+#include "webview_value.h"
+
+#endif //WEBVIEW_PLUGIN_H
--- src/plugins/webview_cef/plugin.c
+++ src/plugins/webview_cef/plugin.c
@@ -4,6 +4,32 @@
 #include "flutter-pi.h"
 #include "pluginregistry.h"
 #include "util/logging.h"
+#include "common/webview_plugin.h"
+#include "keyevent.h"
+#include "texture.h"
+
+static struct plugin {
+    //pthread_mutex_t lock;
+
+    struct flutterpi *flutterpi;
+    bool initialized;
+    //webview_cef::WebviewPlugin *webview;
+} plugin;
+
+static WValue *encode_stdvalue_to_wvalue(struct std_value *args)
+{
+    if(STDVALUE_IS_BOOL(args)) {
+        return webview_value_new_bool(STDVALUE_AS_BOOL(args));
+    }
+
+    if(STDVALUE_IS_INT(args)) {
+        return webview_value_new_int(STDVALUE_AS_INT(args));
+    }
+
+    //TODO Handle everything
+
+    return webview_value_new_null();
+}
 
 static int on_receive(char *channel, struct platch_obj *object, FlutterPlatformMessageResponseHandle *response_handle) {
     (void) channel;
@@ -11,9 +37,9 @@ static int on_receive(char *channel, struct platch_obj *object, FlutterPlatformM
     const char *method;
     method = object->method;
 
-    if (streq(method, "init")) {
-        LOG_ERROR("Shte hendalvame init metoda.\n");
-    }
+    WValue *encodeArgs = encode_stdvalue_to_wvalue(&object->std_arg);
+
+    //TODO Forward to plugin
 
     return platch_respond_not_implemented(response_handle);
 }
@@ -30,6 +56,12 @@ enum plugin_init_result webview_cef_init(struct flutterpi *flutterpi, void **use
 
     *userdata_out = NULL;
 
+    plugin.flutterpi = flutterpi;
+    plugin.initialized = false;
+    //plugin.webview = new webview_cef::WebviewPlugin();
+
+    //TODO Initialize
+
     return PLUGIN_INIT_RESULT_INITIALIZED;
 }
 
@@ -37,6 +69,11 @@ void webview_cef_deinit(struct flutterpi *flutterpi, void *userdata) {
     (void) userdata;
 
     plugin_registry_remove_receiver_v2_locked(flutterpi_get_plugin_registry(flutterpi), WEBVIEW_CEF_CHANNEL);
+
+    //TODO Destroy
+    //if(plugin.webview) {
+    //    free(plugin.webview);
+    //}
 }
 
 FLUTTERPI_PLUGIN("webview cef plugin", webview_cef_plugin, webview_cef_init, webview_cef_deinit)
