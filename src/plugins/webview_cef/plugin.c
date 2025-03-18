5ddacac417ba443d8b0830ade843cbc03baf2d2c
Bojidar Tonchev <bojidar.tonchev@gmail.com>
2025-03-18 12:01:40 +0200

    Update plugin.c

--- src/plugins/webview_cef/plugin.c
+++ src/plugins/webview_cef/plugin.c
@@ -44,7 +44,8 @@ static int on_create(struct platch_obj *object, FlutterPlatformMessageResponseHa
 
     cef_browser_settings_t browser_settings = {};
     cef_string_t url2;
-    cef_string_utf16_set(L"https://www.google.com", wcslen(L"https://www.google.com"), &url2, 1);
+    const wchar_t* url3 = L"https://www.google.com"; 
+    cef_string_utf16_set(url3, wcslen(L"https://www.google.com"), &url2, 1);
 
     cef_browser_host_create_browser(&window_info, NULL, &url2, &browser_settings, NULL, NULL);
 
