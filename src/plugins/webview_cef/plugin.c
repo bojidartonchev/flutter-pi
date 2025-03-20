79cb4e54cfcf20f6124f27292b36653e01627e58
Bojidar Tonchev <bojidar.tonchev@gmail.com>
2025-03-20 16:22:02 +0200

    Update plugin.c

--- src/plugins/webview_cef/plugin.c
+++ src/plugins/webview_cef/plugin.c
@@ -67,8 +67,10 @@ static int on_create(struct platch_obj *object, FlutterPlatformMessageResponseHa
 
     // Create browser asynchronously. There is also a
     // synchronous version of this function available.
-    cef_browser_host_create_browser(&window_info, NULL, &cef_url,
+    cef_browser_t* browser = cef_browser_host_create_browser_sync(&window_info, NULL, &cef_url,
                                     &browser_settings, NULL, NULL);
+
+    LOG_ERROR("Browser id: %d\n", browser->get_identifier())
 
     /*cef_window_info_t window_info = {};
 
